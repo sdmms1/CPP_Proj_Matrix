1b63138e79ff5bfd4870783f928c8efffa82effd
sdmms1 <11813010@mail.sustech.edu.cn>
2020-06-08 11:23:32 +0800

    start the project

---
+++ cmake-build-debug/CMakeFiles/CPP_Proj_Matrix.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_Proj_Matrix.dir/main.cpp.o"
+  "CPP_Proj_Matrix.exe"
+  "CPP_Proj_Matrix.pdb"
+  "libCPP_Proj_Matrix.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_Proj_Matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CPP_Proj_Matrix.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CPP_Proj_Matrix.
+# This may be replaced when dependencies are built.
